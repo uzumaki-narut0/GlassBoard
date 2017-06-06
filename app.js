8f4baee6fef518fa4c16ee1a33761f30d99caf1f
uzumaki-narut0 <kumar.swapnil170@gmail.com>
2017-06-07 01:06:39 +0530

    commiting buggy code

--- app.js
+++ app.js
@@ -13,10 +13,12 @@ app.engine('hbs',hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dir
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+var connection;
 //setting up a rethinkdb database
 r.connect({host:'localhost',port:28015}, function(err, conn){
   if(err)
     throw err;
+  //connecton =conn;
   r.db('test').tableList().run(conn, function(err, response){
     if(response.indexOf('edit') > -1){
       //do nothing..... table is already created
@@ -30,18 +32,38 @@ r.connect({host:'localhost',port:28015}, function(err, conn){
       r.db('test').tableCreate('edit').run(conn);
     }
   });
-});
+
 
 //socket.io stuff here
-io.on('connection', function(socket){
-  console.log('a user connected');
+  io.on('connection', function(socket){
+    console.log('a user connected');
 
-  socket.on('disconnect', function(){
-    console.log(' a user disconnected');
-  })
+    socket.on('disconnect', function(){
+      console.log(' a user disconnected');
+    });
+
+    socket.on('document-update', function(msg){
+      r.table('edit').insert({id:msg.id, value:msg.value, user:msg.user}, {conflict : 'update'}).run(conn, function(err,res){
+        if(err)
+          throw err;
+
+      });
+
+    });
+  });
+
+  r.table('edit').changes().run(conn, function(err, cursor){
+        if(err)
+          throw err;
+        cursor.each(function(err,row){
+          if(err)
+            throw err;
+       //   console.log(row);
+         io.emit('doc',row);
+        });
+      });
+
 });
-
-
 
 
 
@@ -58,12 +80,12 @@ io.on('connection', function(socket){
 app.get('/', function(req, res){
   var id = Math.random().toString(36).slice(2);
   console.log('here');
-  res.redirect('/'+id);
+  res.redirect('/'+id+'/swapnil');
 });
 
-app.get('/:id',function(req,res){
+app.get('/:id/:user',function(req,res){
   console.log(req.params);
-  res.render('index', {id: req.params.id
+  res.render('index', {id: req.params.id, user: req.params.user
   });
   //res.status(200).send(html);
 })
@@ -71,5 +93,5 @@ app.get('/:id',function(req,res){
 
 
 http.listen(portnumber,process.env.IP, function(){
-  console.log('listening on *:' + '172.16.170.72' + ":" + portnumber);
+  console.log('listening on *:' + '172.16.170.18' + ":" + portnumber);
 });
